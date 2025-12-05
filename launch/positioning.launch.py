70d18633e24233f20fdc666a606d6002af2e3b57
Mohammad Elwan <mohammadsherifl11@gmail.com>
2025-12-05 22:06:06 +0100

    6

--- launch/positioning.launch.py
+++ launch/positioning.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument, LogInfo
-from launch_ros.substitutions import ParameterValue
+from launch_ros.parameter_descriptions import ParameterValue
 import os
 from ament_index_python.packages import get_package_share_directory
 
